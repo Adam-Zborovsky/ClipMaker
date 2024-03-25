8b104aa11b540ff079d72af5607c7684616cdd77
Adam Zborovsky <96923903+Adam-Zborovsky@users.noreply.github.com>
2024-03-25 23:09:29 +0200

    1

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from moviepy.editor import VideoFileClip, AudioFileClip
 import os
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
-audio_folder = script_dir + 'resources/audio'
-video_folder = script_dir + 'resources/video'
-output_folder = script_dir + 'output'
+audio_folder = script_dir + '/resources/audio'
+video_folder = script_dir + '/resources/video'
+output_folder = script_dir + '/output'
 
 
 def create_video_clips():
@@ -17,14 +17,16 @@ def create_video_clips():
         #Check if Video is long enough and set Video_file
         video_files = os.listdir(video_folder)
         video_file = video_files[0]
-        if video_file.duration < audio_duration:
+        video_file_duration = VideoFileClip(video_folder +'/'+ video_file).duration
+        
+        if video_file_duration < audio_duration:
             video_file = video_files[1]
-            if video_file.duration < 30:
+            if video_file_duration < 30:
                 os.remove(video_file)
         
 
         # Create a video clip with the same length as the audio file
-        video_clip = VideoFileClip(os.path.join(video_folder, video_file))
+        video_clip = VideoFileClip(video_folder +'/'+ video_file)
         video_clip = video_clip.subclip(0, audio_duration)
         video_clip = video_clip.set_audio(audio_clip)
         output_path = os.path.join(output_folder, f"{audio_file[:-4]}.mp4")
@@ -34,9 +36,13 @@ def create_video_clips():
         remaining_video_clip = video_clip.subclip(audio_duration)
         remaining_video_clip.write_videofile(os.path.join(video_folder, video_file), codec='libx264')
 
-
-
         print(f"Created video clip: {output_path}")
+        os.remove(audio_file)
 
 if __name__ == '__main__':
+    for folder in [audio_folder, video_folder, output_folder]:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+
     create_video_clips()
